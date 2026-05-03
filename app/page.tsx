474b98a650b7eec1d076bfc2a8d8d9412811705c
Maxime Soustelle <soustelle.maxime@gmail.com>
2026-05-03 14:22:57 +0700

    Initial next js setup

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="p-4">
+      <h1 className="text-2xl font-bold">TeachSabai</h1>
+      <p className="mt-2">Your class manager app</p>
+    </main>
+  );
+}
