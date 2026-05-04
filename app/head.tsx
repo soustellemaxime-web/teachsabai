a0578ef0420fd013c156d5e78963962327685634
Maxime Soustelle <soustelle.maxime@gmail.com>
2026-05-04 12:16:35 +0700

    Force light mode for now

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,7 @@ export default function Head() {
     <>
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="color-scheme" content="light" />
     </>
   );
 }
