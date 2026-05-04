6960a2b3d6e85a640043ff0666e76665ecee8a9e
Maxime Soustelle <soustelle.maxime@gmail.com>
2026-05-04 12:07:18 +0700

    PWA

---
+++ app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+    </>
+  );
+}
